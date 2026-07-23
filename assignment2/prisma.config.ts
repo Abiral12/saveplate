f92426f837536ec76975c0743da89ff50edbf1f4
abiral12 <abiralpanta39@gmail.com>
2026-07-23 23:31:53 +0545

    fix(prod1): prisma config file updated.

--- assignment2/prisma.config.ts
+++ assignment2/prisma.config.ts
@@ -1,22 +1,5 @@
-import { config } from "dotenv";
-import { defineConfig } from "prisma/config";
-
-const result = config({
-  path: ".env",
-  override: true,
-});
-
-if (result.error) {
-  throw result.error;
-}
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error(
-    "DATABASE_URL is missing or empty in the root .env file."
-  );
-}
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -26,6 +9,6 @@ export default defineConfig({
   },
 
   datasource: {
-    url: databaseUrl,
+    url: env("DATABASE_URL"),
   },
 });
